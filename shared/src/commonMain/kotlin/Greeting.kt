7fbbe346ce3403631e5f3059ae86bf7ae5ad80f3
Kārlis Bērziņš <karlis.berzins@tet.lv>
2024-05-15 11:55:35 +0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/Greeting.kt
@@ -0,0 +1,37 @@
+import kotlin.random.Random
+
+data class Person(
+    val firstName: String,
+    val lastName: String,
+    val age: Int
+)
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
+
+        return firstWord
+    }
+
+    fun someNewFeature() {
+
+    }
+
+    fun printText() {
+
+    }
+
+    fun afterDeletion() {
+
+    }
+
+    fun getNameFor(person: Person): String {
+        return person.firstName + " " + person.lastName
+    }
+
+    fun createRandomPerson(): Person {
+        return Person(firstName = "John", lastName = "Doe", age = 30)
+    }
+}
